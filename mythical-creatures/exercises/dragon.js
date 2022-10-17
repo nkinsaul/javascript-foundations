dc138b66c7938021b5b93271ec932d065183cf72
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-10-17 15:57:05 -0700

    Complete Dragon.js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,25 @@
 
+class Dragon {
+    constructor (name, rider) {
+        this.name = name;
+        this.rider = rider;
+        this.hungry = true;
+        this.eatCount = 0;
+    }
+    greet() {
+        return `Hi, ${this.rider}!`;
+    }
+    eat() {
+        if (this.eatCount<2) {
+            this.eatCount++;
+        } else {
+            this.hungry = false;
+        }  
+    }
+}
+
+
+
+
+
+module.exports = Dragon;
