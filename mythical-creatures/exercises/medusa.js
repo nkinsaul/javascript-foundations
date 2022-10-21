f27d98d56cae60e443c880b165c4be590abd25e9
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-10-21 14:51:06 -0700

    Complete Medusa-test

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,25 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+    constructor(name) {
+        this.name = name;
+        this.statues = [];
+    }
+    gazeAtVictim(victim) {
+        var newStatue = new Statue(victim.name) 
+        this.statues.push(newStatue);
+        if (this.statues.length > 3) {
+            var freeStatue = this.statues.shift()
+            var returnPerson = new Person(freeStatue.name);
+            returnPerson.mood = 'relieved';
+            return returnPerson;
+        }
+    }
+}
+
+
+module.exports = Medusa; 
+
+
+
